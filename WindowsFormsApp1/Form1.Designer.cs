19f607da8c15ff3d6530846741c79342972f6d65
SSS-ysc <531842930@qq.com>
2022-02-08 17:14:49 +0800

    1.进一步优化UI，仅将程序作为解析功能

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,204 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            this.uiSymbolButtonOpenFile = new Sunny.UI.UISymbolButton();
-            this.uiSymbolButtonSaveFile = new Sunny.UI.UISymbolButton();
-            this.uiStyleManager1 = new Sunny.UI.UIStyleManager(this.components);
-            this.uiTextBoxFile = new Sunny.UI.UITextBox();
-            this.uiRadioButtonGroup1 = new Sunny.UI.UIRadioButtonGroup();
-            this.uiCheckBox1 = new Sunny.UI.UICheckBox();
-            this.uiSymbolButton1 = new Sunny.UI.UISymbolButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // uiSymbolButtonOpenFile
+            // textBox1
             // 
-            this.uiSymbolButtonOpenFile.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.uiSymbolButtonOpenFile.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
-            this.uiSymbolButtonOpenFile.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
-            this.uiSymbolButtonOpenFile.FillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiSymbolButtonOpenFile.Font = new System.Drawing.Font("微软雅黑", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiSymbolButtonOpenFile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiSymbolButtonOpenFile.Location = new System.Drawing.Point(348, 46);
-            this.uiSymbolButtonOpenFile.MinimumSize = new System.Drawing.Size(1, 1);
-            this.uiSymbolButtonOpenFile.Name = "uiSymbolButtonOpenFile";
-            this.uiSymbolButtonOpenFile.Radius = 10;
-            this.uiSymbolButtonOpenFile.RectHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiSymbolButtonOpenFile.Size = new System.Drawing.Size(101, 32);
-            this.uiSymbolButtonOpenFile.Style = Sunny.UI.UIStyle.Custom;
-            this.uiSymbolButtonOpenFile.Symbol = 61717;
-            this.uiSymbolButtonOpenFile.SymbolColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiSymbolButtonOpenFile.TabIndex = 1;
-            this.uiSymbolButtonOpenFile.TabStop = false;
-            this.uiSymbolButtonOpenFile.Text = "浏览文件";
-            this.uiSymbolButtonOpenFile.TipsFont = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiSymbolButtonOpenFile.Click += new System.EventHandler(this.Open_File_Click);
+            this.textBox1.Location = new System.Drawing.Point(23, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(350, 25);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            this.textBox1.WordWrap = false;
             // 
-            // uiSymbolButtonSaveFile
+            // button1
             // 
-            this.uiSymbolButtonSaveFile.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.uiSymbolButtonSaveFile.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
-            this.uiSymbolButtonSaveFile.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
-            this.uiSymbolButtonSaveFile.FillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiSymbolButtonSaveFile.Font = new System.Drawing.Font("微软雅黑", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiSymbolButtonSaveFile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiSymbolButtonSaveFile.Location = new System.Drawing.Point(455, 46);
-            this.uiSymbolButtonSaveFile.MinimumSize = new System.Drawing.Size(1, 1);
-            this.uiSymbolButtonSaveFile.Name = "uiSymbolButtonSaveFile";
-            this.uiSymbolButtonSaveFile.Radius = 10;
-            this.uiSymbolButtonSaveFile.RectHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiSymbolButtonSaveFile.Size = new System.Drawing.Size(101, 32);
-            this.uiSymbolButtonSaveFile.Style = Sunny.UI.UIStyle.Custom;
-            this.uiSymbolButtonSaveFile.Symbol = 61639;
-            this.uiSymbolButtonSaveFile.SymbolColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiSymbolButtonSaveFile.TabIndex = 2;
-            this.uiSymbolButtonSaveFile.TabStop = false;
-            this.uiSymbolButtonSaveFile.Text = "导出文件";
-            this.uiSymbolButtonSaveFile.TipsFont = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiSymbolButtonSaveFile.Click += new System.EventHandler(this.Save_File_Click);
+            this.button1.Location = new System.Drawing.Point(382, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "浏览文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Open_File_Click);
             // 
-            // uiStyleManager1
+            // button2
             // 
-            this.uiStyleManager1.Style = Sunny.UI.UIStyle.LightBlue;
+            this.button2.Location = new System.Drawing.Point(124, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "保存";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Save_File_Click);
             // 
-            // uiTextBoxFile
+            // button3
             // 
-            this.uiTextBoxFile.ButtonFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
-            this.uiTextBoxFile.ButtonFillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiTextBoxFile.ButtonForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiTextBoxFile.ButtonRectHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiTextBoxFile.ButtonSymbol = 61761;
-            this.uiTextBoxFile.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.uiTextBoxFile.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
-            this.uiTextBoxFile.Font = new System.Drawing.Font("微软雅黑", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiTextBoxFile.Location = new System.Drawing.Point(19, 46);
-            this.uiTextBoxFile.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.uiTextBoxFile.Maximum = 2147483647D;
-            this.uiTextBoxFile.Minimum = -2147483648D;
-            this.uiTextBoxFile.MinimumSize = new System.Drawing.Size(1, 15);
-            this.uiTextBoxFile.Name = "uiTextBoxFile";
-            this.uiTextBoxFile.Radius = 10;
-            this.uiTextBoxFile.Size = new System.Drawing.Size(323, 27);
-            this.uiTextBoxFile.Style = Sunny.UI.UIStyle.Custom;
-            this.uiTextBoxFile.TabIndex = 0;
-            this.uiTextBoxFile.TabStop = false;
-            this.uiTextBoxFile.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(382, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "反编译测试";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.Decompile_Click);
             // 
-            // uiRadioButtonGroup1
+            // checkBox1
             // 
-            this.uiRadioButtonGroup1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.uiRadioButtonGroup1.ColumnCount = 3;
-            this.uiRadioButtonGroup1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
-            this.uiRadioButtonGroup1.Font = new System.Drawing.Font("微软雅黑", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiRadioButtonGroup1.Items.AddRange(new object[] {
-            "Base",
-            "CEA",
-            "DisplayID"});
-            this.uiRadioButtonGroup1.ItemSize = new System.Drawing.Size(90, 18);
-            this.uiRadioButtonGroup1.Location = new System.Drawing.Point(348, 106);
-            this.uiRadioButtonGroup1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.uiRadioButtonGroup1.MinimumSize = new System.Drawing.Size(1, 1);
-            this.uiRadioButtonGroup1.Name = "uiRadioButtonGroup1";
-            this.uiRadioButtonGroup1.Padding = new System.Windows.Forms.Padding(0, 20, 0, 0);
-            this.uiRadioButtonGroup1.Size = new System.Drawing.Size(300, 58);
-            this.uiRadioButtonGroup1.Style = Sunny.UI.UIStyle.Custom;
-            this.uiRadioButtonGroup1.StyleCustomMode = true;
-            this.uiRadioButtonGroup1.TabIndex = 4;
-            this.uiRadioButtonGroup1.TabStop = false;
-            this.uiRadioButtonGroup1.Text = null;
-            this.uiRadioButtonGroup1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
-            this.uiRadioButtonGroup1.Visible = false;
-            this.uiRadioButtonGroup1.ValueChanged += new Sunny.UI.UIRadioButtonGroup.OnValueChanged(this.uiRadioButtonGroup1_ValueChanged);
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(230, 72);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 19);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "厂内格式";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
-            // uiCheckBox1
+            // button4
             // 
-            this.uiCheckBox1.Checked = true;
-            this.uiCheckBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.uiCheckBox1.Font = new System.Drawing.Font("微软雅黑", 8F);
-            this.uiCheckBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiCheckBox1.Location = new System.Drawing.Point(562, 51);
-            this.uiCheckBox1.MinimumSize = new System.Drawing.Size(1, 1);
-            this.uiCheckBox1.Name = "uiCheckBox1";
-            this.uiCheckBox1.Padding = new System.Windows.Forms.Padding(22, 0, 0, 0);
-            this.uiCheckBox1.Size = new System.Drawing.Size(129, 27);
-            this.uiCheckBox1.Style = Sunny.UI.UIStyle.Custom;
-            this.uiCheckBox1.StyleCustomMode = true;
-            this.uiCheckBox1.TabIndex = 3;
-            this.uiCheckBox1.TabStop = false;
-            this.uiCheckBox1.Text = "厂内格式";
-            // 
-            // uiSymbolButton1
-            // 
-            this.uiSymbolButton1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.uiSymbolButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
-            this.uiSymbolButton1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
-            this.uiSymbolButton1.FillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiSymbolButton1.Font = new System.Drawing.Font("微软雅黑", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiSymbolButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiSymbolButton1.Location = new System.Drawing.Point(537, 427);
-            this.uiSymbolButton1.MinimumSize = new System.Drawing.Size(1, 1);
-            this.uiSymbolButton1.Name = "uiSymbolButton1";
-            this.uiSymbolButton1.Radius = 10;
-            this.uiSymbolButton1.RectHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiSymbolButton1.Size = new System.Drawing.Size(111, 32);
-            this.uiSymbolButton1.Style = Sunny.UI.UIStyle.Custom;
-            this.uiSymbolButton1.Symbol = 61473;
-            this.uiSymbolButton1.SymbolColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
-            this.uiSymbolButton1.TabIndex = 5;
-            this.uiSymbolButton1.TabStop = false;
-            this.uiSymbolButton1.Text = "Decompile";
-            this.uiSymbolButton1.TipsFont = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiSymbolButton1.Click += new System.EventHandler(this.uiSymbolButton1_Click);
+            this.button4.Location = new System.Drawing.Point(23, 66);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 29);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "解析";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Decode_Click);
             // 
             // Form1
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(678, 478);
-            this.ControlBoxCloseFillHoverColor = System.Drawing.Color.White;
-            this.ControlBoxFillHoverColor = System.Drawing.Color.White;
-            this.ControlBoxForeColor = System.Drawing.Color.Black;
-            this.Controls.Add(this.uiSymbolButton1);
-            this.Controls.Add(this.uiCheckBox1);
-            this.Controls.Add(this.uiRadioButtonGroup1);
-            this.Controls.Add(this.uiTextBoxFile);
-            this.Controls.Add(this.uiSymbolButtonSaveFile);
-            this.Controls.Add(this.uiSymbolButtonOpenFile);
-            this.Font = new System.Drawing.Font("微软雅黑", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ClientSize = new System.Drawing.Size(507, 119);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(1851, 1224);
             this.Name = "Form1";
-            this.Padding = new System.Windows.Forms.Padding(0, 31, 0, 0);
-            this.ShowRadius = false;
-            this.ShowRect = false;
-            this.ShowTitleIcon = true;
-            this.Style = Sunny.UI.UIStyle.Custom;
-            this.StyleCustomMode = true;
             this.Text = "BOE EDID Tool V1.0";
-            this.TitleFont = new System.Drawing.Font("微软雅黑", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.TitleHeight = 31;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
-        private Sunny.UI.UISymbolButton uiSymbolButtonOpenFile;
-        private Sunny.UI.UISymbolButton uiSymbolButtonSaveFile;
-        private Sunny.UI.UIStyleManager uiStyleManager1;
-        private Sunny.UI.UITextBox uiTextBoxFile;
-        private Sunny.UI.UIRadioButtonGroup uiRadioButtonGroup1;
-        private Sunny.UI.UICheckBox uiCheckBox1;
-        private Sunny.UI.UISymbolButton uiSymbolButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,60 +1,20 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
 using System.Configuration;
-
-using Sunny.UI;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
 using EDIDApp;
 
 namespace WindowsFormsApp_BOE_Tool
 {
-    public partial class Form1 : UIForm
+    public partial class Form1 : Form
     {
-        Sunny.UI.UITextBox[] EDIDTextBox = new Sunny.UI.UITextBox[128];
         EDID FormEDID = new EDID();
         EDIDTable EDIDInfo = new EDIDTable();
 
         public Form1()
         {
             InitializeComponent();
-
-            //自编的控件
-            int BoxSizeWeight = 28;
-            int BoxSizeHeight = 22;
-            int BoxSpaceX = 3;
-            int BoxSpaceY = 3;
-            for (int i = 0; i < EDIDTextBox.Length; i++)
-            {
-                // 
-                // uiTextBox
-                // 
-                EDIDTextBox[i] = new Sunny.UI.UITextBox();
-                EDIDTextBox[i].ButtonSymbol = 61761;
-                EDIDTextBox[i].Cursor = System.Windows.Forms.Cursors.IBeam;
-                EDIDTextBox[i].Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-                EDIDTextBox[i].Location = new System.Drawing.Point(22 + (i % 10) * (BoxSizeWeight + BoxSpaceX), 115 + (i / 10) * (BoxSizeHeight + BoxSpaceY));
-                EDIDTextBox[i].Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-                EDIDTextBox[i].Maximum = 2147483647D;
-                EDIDTextBox[i].Minimum = -2147483648D;
-                EDIDTextBox[i].MinimumSize = new System.Drawing.Size(1, 16);
-                EDIDTextBox[i].Name = "EDIDTextBox_" + i.ToString();
-                EDIDTextBox[i].Size = new System.Drawing.Size(BoxSizeWeight, BoxSizeHeight);
-                EDIDTextBox[i].Style = Sunny.UI.UIStyle.Custom;
-                EDIDTextBox[i].TabStop = false;
-                EDIDTextBox[i].Text = "";
-                EDIDTextBox[i].TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
-                EDIDTextBox[i].ReadOnly = true;
-                Controls.Add(EDIDTextBox[i]);
-            }
         }
         private void Open_File_Click(object sender, EventArgs e)
         {
@@ -67,61 +27,46 @@ namespace WindowsFormsApp_BOE_Tool
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 string filePath = openFileDialog1.FileName;
-                string UnicodeText = "";
-                uiTextBoxFile.Text = filePath;
+                textBox1.Text = filePath;
 
-                using (FileStream fsRead = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-                {
-                    byte[] b = new byte[50];
-                    while (true)
-                    {
-                        int r = fsRead.Read(b, 0, b.Length);
-                        if (r == 0)
-                            break;
-                        UnicodeText += Encoding.UTF8.GetString(b, 0, r);
-                    }
-                }
+                button4.Text = "解析";
 
-                EDIDInfo = FormEDID.Decode(UnicodeText);
-
-                if (EDIDInfo.Error == DecodeError.Success)
-                {
-                    uiRadioButtonGroup1.Items.Clear();
-                    if (EDIDInfo.Length == 384)
-                    {
-                        uiRadioButtonGroup1.Items.AddRange(new object[] { "Base", "CEA", "DisplayID" });
-                    }
-                    if (EDIDInfo.Length == 256)
-                    {
-                        uiRadioButtonGroup1.Items.AddRange(new object[] { "Base", "CEA" });
-                    }
-                    if (EDIDInfo.Length == 128)
-                    {
-                        uiRadioButtonGroup1.Items.AddRange(new object[] { "Base" });
-                    }
-
-                    uiRadioButtonGroup1.SelectedIndex = 0;
-                    for (int i = 0; i < 128; i++)
-                    {
-                        EDIDTextBox[i].Text = string.Format("{0:X2}", EDIDInfo.Data[i]);
-                    }
-                    uiRadioButtonGroup1.Visible = true;
-                }
-                else
-                {
-                    uiRadioButtonGroup1.Visible = false;
-                    MessageBox.Show(EDIDInfo.Error.ToString(), "EDID解析错误");
-                }
                 //窗体关闭时，获取文件夹对话框的路径写入配置文件中
                 Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
                 config.AppSettings.Settings["EDIDFilePath"].Value = filePath;
                 config.Save(ConfigurationSaveMode.Modified);
             }
         }
+        private void Decode_Click(object sender, EventArgs e)
+        {
+            string UnicodeText = "";
+            using (FileStream fsRead = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read))
+            {
+                byte[] b = new byte[50];
+                while (true)
+                {
+                    int r = fsRead.Read(b, 0, b.Length);
+                    if (r == 0)
+                        break;
+                    UnicodeText += Encoding.UTF8.GetString(b, 0, r);
+                }
+            }
+
+            EDIDInfo = FormEDID.Decode(UnicodeText);
+
+            if (EDIDInfo.Error != DecodeError.Success)
+            {
+                MessageBox.Show(EDIDInfo.Error.ToString(), "EDID解析错误");
+            }
+            else
+            {
+                button4.Text = "解析成功";
+            }
+        }
         private void Save_File_Click(object sender, EventArgs e)
         {
             saveFileDialog1.InitialDirectory = "@" + ConfigurationManager.AppSettings["EDIDFilePath"];
-            saveFileDialog1.RestoreDirectory= false;
+            saveFileDialog1.RestoreDirectory = false;
             saveFileDialog1.Filter = ".txt文件(.txt)|*.txt |.h文件（.h）|*.h";
             saveFileDialog1.FilterIndex = 1;
             saveFileDialog1.FileName = "EDID_test";
@@ -130,7 +75,7 @@ namespace WindowsFormsApp_BOE_Tool
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 EDID FormEDID = new EDID();
-                if (uiCheckBox1.Checked == true)
+                if (checkBox1.Checked == true)
                     FormEDID.OutputNotesEDIDText(EDIDInfo, saveFileDialog1.FileName);
                 else
                     FormEDID.Output0xEDIDText(EDIDInfo, saveFileDialog1.FileName);
@@ -141,32 +86,8 @@ namespace WindowsFormsApp_BOE_Tool
                 config.Save(ConfigurationSaveMode.Modified);
             }
         }
-        private void uiRadioButtonGroup1_ValueChanged(object sender, int index, string text)
-        {
-            if (index == 0)
-            {
-                for (int i = 0; i < 128; i++)
-                {
-                    EDIDTextBox[i].Text = string.Format("{0:X2}", EDIDInfo.Data[i]);
-                }
-            }
-            else if ((index == 1) && (EDIDInfo.Length >= 128))
-            {
-                for (int i = 0; i < 128; i++)
-                {
-                    EDIDTextBox[i].Text = string.Format("{0:X2}", EDIDInfo.Data[128 + i]);
-                }
-            }
-            else if ((index == 2) && (EDIDInfo.Length >= 256))
-            {
-                for (int i = 0; i < 128; i++)
-                {
-                    EDIDTextBox[i].Text = string.Format("{0:X2}", EDIDInfo.Data[256 + i]);
-                }
-            }
-        }
 
-        private void uiSymbolButton1_Click(object sender, EventArgs e)
+        private void Decompile_Click(object sender, EventArgs e)
         {
             EDID FormEDID = new EDID();
             FormEDID.Decompile(EDIDInfo);
